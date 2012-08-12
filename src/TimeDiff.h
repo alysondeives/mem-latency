e696240f7bdc63a010d1190abfe09ea1cf50240d
Pete Stevenson <etep.nosnevets@gmail.com>
2012-08-12 18:59:47 -0500

    TimeDiff.h: fixed function signature for DiffTimespec()

--- src/TimeDiff.h
+++ src/TimeDiff.h
@@ -8,5 +8,5 @@
 #include <mach/mach_time.h>
 void clock_gettime( const U32 unused, timespec * tsp );
 #endif
-double DiffTimespec( const timespec * beg, const timespec * end );
+double DiffTimespec( const timespec * beg, const timespec * end, const double numtrials );
 void AnalyzeRunTimeWallTime( const timespec * clks, const TestParams & params, U64 size, const U32 numTrials );
